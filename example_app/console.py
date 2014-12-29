eee249575008507f0a74f9b687237a8d7cd9a057
Allen Bierbaum <abierbaum@gmail.com>
2014-12-29 15:00:10 -0600

    Update example app to try to modify an image.

--- example_app/console.py
+++ example_app/console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 from rauth import OAuth1Session
 import sys
 
@@ -17,8 +18,36 @@ def main():
                        params={'oauth_verifier': verifier})
     session = OAuth1Session(API_KEY, API_KEY_SECRET, access_token=at,
                                  access_token_secret=ats)
+    print("Access Token: ", at)
+    print("Access Token Secret: ", ats)
+
     print(session.get(API_ORIGIN + '/api/v2!authuser',
                     headers={'Accept': 'application/json'}).text)
+
+    # Pick the first image in recent images and change the caption
+    if 1:
+        headers = {'Accept': 'application/json'}
+
+        new_caption = "Test Caption"
+        r = session.get(API_ORIGIN + '/api/v2!authuser', headers = headers)
+        r_obj = r.json()
+
+        user_obj = r_obj['Response']['User']
+        user_uris = user_obj['Uris']
+        recent_images_uri = user_uris['UserRecentImages']['Uri']
+
+        r = session.get(API_ORIGIN + recent_images_uri, headers = headers)
+        r_obj = r.json()
+        image_objs = r_obj['Response']['Image']
+
+        first_image = image_objs[0]
+        image_uri = first_image['Uri']
+
+        # change the image caption
+        r = session.post(API_ORIGIN + image_uri,
+                        data = json.dumps({'Caption': new_caption}), headers = headers)
+        print("Post result")
+        print(r.content)
 
 if __name__ == '__main__':
     main()
